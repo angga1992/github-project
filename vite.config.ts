b2cd53b1337785de88013bfd15757a0887057217
angga1992 <anggamulya.ghazy@gmail.com>
2025-08-01 18:53:26 +0700

    terser vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,19 +5,19 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
     rollupOptions: {
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
           markdown: ['marked', 'dompurify'],
         },
-      },
-    },
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
       },
     },
   },
